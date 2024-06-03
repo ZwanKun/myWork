e5398387aa5e6011c7e62ce6a82fce9b4f45e1ec
Iwan <elkhamlichiiwan@gmail.com>
2024-06-03 15:12:15 +0200

    nothing changed

--- portfolio.js
+++ portfolio.js
@@ -2,30 +2,15 @@
 document.querySelector('.scroll-mouse').addEventListener('click',function(){
     document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
 })
+// scroll contact button 
+document.querySelector('.btn').addEventListener('click',function(){
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+})
 
-//skill front//
-document.getElementById('Front').addEventListener('click', function() {
-    let skillList = document.querySelectorAll('#Front + ul li');
-    skillList.forEach(function(item) {
-        if (item.style.display === "none") {
-            item.style.display = "block";
-        } else {
-            item.style.display = "none";
-        }
-    });
-});
 
-//skill design//
-document.getElementById('Design').addEventListener('click', function() {
-    let skillList = document.querySelectorAll('#Design + ul li');
-    skillList.forEach(function(list) {
-        if (list.style.display === "none") {
-            list.style.display = "block";
-        } else {
-            list.style.display = "none";
-        }
-    });
-});
+
+
+
 
 
 
